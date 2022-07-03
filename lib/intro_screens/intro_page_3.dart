49ca64a54968e88eb0277e91cf5a9169ece4b1b0
omerlevennt <omerlevennt@gmail.com>
2022-07-03 21:19:47 +0300

    Initial commit

---
+++ lib/intro_screens/intro_page_3.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IntroPage3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepOrange[100],
+      child: Center(child: Text("PAGE 3")),
+    );
+  }
+}
---
+++ lib/onBoardingScreen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import 'home_page.dart';
+import 'intro_screens/intro_page_1.dart';
+import 'intro_screens/intro_page_2.dart';
+import 'intro_screens/intro_page_3.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  PageController _controller = PageController();
+
+  bool onLastPage = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        PageView(
+          onPageChanged: (index) {
+            setState(() {
+              onLastPage = (index == 2);
+            });
+          },
+          controller: _controller,
+          children: [
+            IntroPage1(),
+            IntroPage2(),
+            IntroPage3(),
+          ],
+        ),
+        Container(
+            alignment: Alignment(0, 0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // skip
+                GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(2);
+                    },
+                    child: Text("Skip")),
+                SmoothPageIndicator(
+                  controller: _controller,
+                  count: 3,
+                  effect: ExpandingDotsEffect(
+                    activeDotColor: Colors.deepPurple,
+                    dotColor: Colors.deepPurple.shade100,
+                    dotHeight: 12,
+                    dotWidth: 12,
+                    spacing: 12,
+                  ),
+                ),
+                //next or done
+                onLastPage
+                    ? GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return HomePage();
+                          }));
+                        },
+                        child: Text("Done"))
+                    : GestureDetector(
+                        onTap: () {
+                          _controller.nextPage(
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeIn);
+                        },
+                        child: Text("Next")),
+              ],
+            ))
+      ],
+    ));
+  }
+}
